590765da41168774762b56e02e176601434c0b25
Deniss <619deniss61999@gmail.com>
2020-09-05 16:54:01 +0300

    BET-12 Implement parsers for marathonbet corrected notes after review 2

--- betScanner.py
+++ betScanner.py
@@ -6,30 +6,30 @@ from selenium import webdriver
 def main():
     driver = webdriver.Chrome()
     driver.get("https://www.marathonbet.ru/su/")
-    btn_tennis = driver.find_element_by_class_name("icon-sport-tennis")
-    btn_tennis.click()
+    driver.find_element_by_class_name("icon-sport-tennis").click()
     start_time = time.time()
     file_log = logging.FileHandler('Log.log')
     console_out = logging.StreamHandler()
     logging.basicConfig(handlers=(file_log, console_out), format='[%(asctime)s | %(levelname)s]: %(message)s',
                         datefmt='%m.%d.%Y %H:%M:%S', level=logging.INFO)
-    matches = list(filter(delete_bets_without_odds, map(massive_of_matches, massive_of_table_strings(driver))))
+    matches = list(filter(has_rates, map(has_list_matches, has_list_table_rows(driver))))
     print(matches)
     logging.info('time for script execution %s seconds' % str(time.time() - start_time))
     driver.close()
     return len(matches)
 
 
-def delete_bets_without_odds(table_string):
+def has_rates(table_string):
     if (table_string['K1'] or table_string['K2']) == '—':
-        return 0
+        return False
     else:
-        return 1
+        return True
 
 
-def massive_of_matches(table_string):
-    player1 = (table_string.find_element_by_xpath(".//tr[1]")).find_element_by_class_name("member-link")
-    player2 = (table_string.find_element_by_xpath(".//tr[2]")).find_element_by_class_name("member-link")
+def has_list_matches(table_string):
+    name_row = "member-link"
+    player1 = (table_string.find_element_by_xpath(".//tr[1]")).find_element_by_class_name(name_row)
+    player2 = (table_string.find_element_by_xpath(".//tr[2]")).find_element_by_class_name(name_row)
     match = player1.text.replace(',', '') + ' VS ' + player2.text.replace(',', '')
     k1 = table_string.find_element_by_xpath(".//td[contains(@class,'first-in-main-row')]").text
     k2 = table_string.find_element_by_xpath(
@@ -37,7 +37,7 @@ def massive_of_matches(table_string):
     return dict(match=match, K1=k1, K2=k2)
 
 
-def massive_of_table_strings(driver):
+def has_list_table_rows(driver):
     location = 0
     while True:
         table_mass = driver.find_elements_by_xpath("//table[@class='coupon-row-item']")
