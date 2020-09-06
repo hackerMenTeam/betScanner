214a0353c82e28b6491b2960b17ce65ca9f0936e
Deniss <619deniss61999@gmail.com>
2020-09-06 13:33:04 +0300

    BET-12 Implement parsers for marathonbet corrected function names, aded constants

--- betScanner.py
+++ betScanner.py
@@ -11,8 +11,7 @@ def main():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options)
-
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get("https://www.marathonbet.ru/su/")
     driver.find_element_by_class_name("icon-sport-tennis").click()
     start_time = time.time()
@@ -20,7 +19,7 @@ def main():
     console_out = logging.StreamHandler()
     logging.basicConfig(handlers=(file_log, console_out), format='[%(asctime)s | %(levelname)s]: %(message)s',
                         datefmt='%m.%d.%Y %H:%M:%S', level=logging.INFO)
-    matches = list(filter(has_rates, map(has_list_matches, has_list_table_rows(driver))))
+    matches = list(filter(has_rates, map(get_list_matches, get_table_rows(driver))))
     print(matches)
     logging.info('time for script execution %s seconds' % str(time.time() - start_time))
     driver.close()
@@ -34,28 +33,32 @@ def has_rates(table_string):
         return True
 
 
-def has_list_matches(table_string):
+def get_list_matches(table_string):
     name_row = "member-link"
-    player1 = (table_string.find_element_by_xpath(".//tr[1]")).find_element_by_class_name(name_row)
-    player2 = (table_string.find_element_by_xpath(".//tr[2]")).find_element_by_class_name(name_row)
-    match = player1.text.replace(',', '') + ' VS ' + player2.text.replace(',', '')
-    k1 = table_string.find_element_by_xpath(".//td[contains(@class,'first-in-main-row')]").text
-    k2 = table_string.find_element_by_xpath(
-        ".//td[contains(@class,'price') and not(contains(@class,'first-in-main-row'))]").text
-    return dict(match=match, K1=k1, K2=k2)
+    player_1 = (table_string.find_element_by_xpath(".//tr[1]")).find_element_by_class_name(name_row)
+    player_2 = (table_string.find_element_by_xpath(".//tr[2]")).find_element_by_class_name(name_row)
+    match = player_1.text.replace(',', '') + ' VS ' + player_2.text.replace(',', '')
+    CLASS_K_1 = 'first-in-main-row'
+    XPATH_K_1 = ".//td[contains(@class,'%s')]" % CLASS_K_1
+    XPATH_K_2 = ".//td[contains(@class,'price') and not(contains(@class,'%s'))]" % CLASS_K_1
+    k_1 = table_string.find_element_by_xpath(XPATH_K_1).text
+    k_2 = table_string.find_element_by_xpath(XPATH_K_2).text
+    return dict(match=match, K1=k_1, K2=k_2)
 
 
-def has_list_table_rows(driver):
+def get_table_rows(driver):
     location = 0
     while True:
-        table_mass = driver.find_elements_by_xpath("//table[@class='coupon-row-item']")
+        XPATH_TABLE_MATCHES = "//table[@class='coupon-row-item']"
+        table_mass = driver.find_elements_by_xpath(XPATH_TABLE_MATCHES)
         if location != table_mass[-1]:
-            driver.execute_script("arguments[0].scrollIntoView();", table_mass[-1])
+            SCRIPT_SCROLL_INTO_VIEW = "arguments[0].scrollIntoView();"
+            driver.execute_script(SCRIPT_SCROLL_INTO_VIEW, table_mass[-1])
             location = table_mass[-1]
-            time.sleep(0.1)
-            active_requests = driver.execute_script('return jQuery.active == 0')
+            SCRIPT_ACTIVE_AJAX = 'return jQuery.active == 0'
+            active_requests = driver.execute_script(SCRIPT_ACTIVE_AJAX)
             while active_requests is not True:
-                active_requests = driver.execute_script('return jQuery.active == 0')
+                active_requests = driver.execute_script(SCRIPT_ACTIVE_AJAX)
                 time.sleep(0.1)
         else:
             return table_mass
