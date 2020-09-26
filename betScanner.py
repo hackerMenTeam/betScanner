eea10f50cb9bdbe83878a21cc2400a503ea6d7f3
Kirill <kolhoz16>
2020-09-26 23:02:41 +0300

    Implemented the launch of modules through asyncio

--- betScanner.py
+++ betScanner.py
@@ -2,25 +2,22 @@ import os
 from selenium import webdriver
 import TennisMarathonBetModule
 import Tennis1xbetModule
-import TennisVulkanbetModule
-import threading
 import logging
 import time
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
 
 
 def main():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--log-level=3")
     driver_1xbet = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver_marathonbet = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    threading.Thread(target=start_1xbet(driver_1xbet)).start()
-    threading.Thread(target=start_marathonbet(driver_marathonbet)).start()
-    driver_1xbet.close()
-    driver_marathonbet.close()
+    read_data([start_1xbet, start_marathonbet], [driver_1xbet, driver_marathonbet])
 
 
 def start_marathonbet(driver):
@@ -34,6 +31,8 @@ def start_marathonbet(driver):
     matches = dict(bookmaker_key='1xbet', matches=tennis.get_matches())
     print(matches)
     logging.info('time for script execution %s seconds' % str(time.time() - start_time))
+    driver.close()
+    return matches
 
 
 def start_1xbet(driver):
@@ -47,6 +46,19 @@ def start_1xbet(driver):
     matches = dict(bookmaker_key='marathonbet', matches=tennis.get_matches())
     print(matches)
     logging.info('time for script execution %s seconds' % str(time.time() - start_time))
+    driver.close()
+    return matches
+
+
+def read_data(scanners, drivers):
+    loop_answer = []
+    executor = ThreadPoolExecutor(10)
+    loop = asyncio.get_event_loop()
+    for i in range(len(scanners)):
+        loop_answer.append(loop.run_in_executor(executor, scanners[i], drivers[i]))
+    loop.run_until_complete(asyncio.gather(*loop_answer))
+    print("Ответ ", loop_answer)
+    return loop_answer
 
 
 if __name__ == "__main__":
