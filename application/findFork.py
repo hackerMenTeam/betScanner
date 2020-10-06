13f16de27858d78f873759051557e4b654de4bfc
Artyom Kakayev <artyom25kakayev@gmail.com>
2020-10-06 21:20:50 +0300

    BET-36 Implement logic to determine forks upgrade function and added test

--- application/findFork.py
+++ application/findFork.py
@@ -25,108 +25,35 @@ bets = [
 
 def sort_fork(list):
     for i in list:
-        if i.exodus=='win_1':
-            first=i
-        elif i.exodus=='win_2':
-            second=i
+        if i.exodus == 'win_1':
+            first = i
+        elif i.exodus == 'win_2':
+            second = i
         else:
-            third=i
-    return [first,second,third]
+            third = i
+    return [first, second, third]
+
 
 def find_forks(bets):
-    a = []
-    for current_match in bets:
-        round = 0
-        fork = []
-        for i in range(3):
-            if round == 0:
-                coef_1 = 0
-                coef_2 = 1
-                coef_3 = 2
-            elif (round == 2):
-                coef_1 = 1
-                coef_2 = 2
-                coef_3 = 0
+    forks = []
+    for first_bet in bets:
+
+        for second_bet in bets:
+            if len(first_bet) < 3:
+                if first_bet[0].bookmaker != second_bet[1].bookmaker:
+                    if (1 / first_bet[0].coef + 1 / second_bet[1].coef < 1):
+                        forks.append(Fork([first_bet[0], second_bet[1]], time.time()))
             else:
-                coef_1 = 2
-                coef_2 = 0
-                coef_3 = 1
-            for second_bet in bets:
-                if len(current_match) < 3:
-                    if current_match[coef_1].bookmaker != second_bet[coef_2].bookmaker:
-                        if (1 / current_match[coef_1].coef + 1 / second_bet[coef_2].coef < 1):
-                            fork.append(Fork([current_match[coef_1], second_bet[coef_2]], time.time()))
-                else:
-                    for third_bet in bets:
-                        if (current_match[coef_1].bookmaker != second_bet[coef_2].bookmaker != third_bet[
-                            coef_3].bookmaker):
-                            if (1 / current_match[coef_1].coef + 1 / second_bet[coef_2].coef + 1 / third_bet[
-                                coef_3].coef < 1):
-                                fork.append(
-                                    Fork(sort_fork([current_match[coef_1], second_bet[coef_2], third_bet[coef_3]]), time.time()))
-            round += 1
-            a.append(fork)
-    a = flat_list(list(filter(lambda x: len(x) > 0, a)))
-    print(len(a))
-    print('a', a)
+                for third_bet in bets:
+                    if not(first_bet[0].bookmaker == second_bet[1].bookmaker == third_bet[2].bookmaker):
+                        if (1 / first_bet[0].coef + 1 / second_bet[1].coef + 1 / third_bet[2].coef < 1):
+                            forks.append(Fork(sort_fork([first_bet[0], second_bet[1], third_bet[2]]), time.time()))
+    forks = flat_list(forks)
     res = []
-    for i in a:
+    for i in forks:
         if i not in res:
             res.append(i)
-    print(len(res))
-    for i in res:
-        print(i)
-    return fork
-    # return flat_list(list(filter(lambda x: len(x) > 0, map(lambda bets: find_forks_for_bet(bets, bets), bets))))
-
-
-def find_forks_for_bet(matches, current_match):
-    print(matches)
-    fork = []
-    round = 0
-    if round == 0:
-        coef_1 = 0
-        coef_2 = 1
-        coef_3 = 2
-    elif (round == 2):
-        coef_1 = 1
-        coef_2 = 2
-        coef_3 = 0
-    else:
-        coef_1 = 2
-        coef_2 = 0
-        coef_3 = 1
-    for second_bet in matches:
-        if len(current_match) < 3:
-            if current_match[coef_1].bookmaker != second_bet[coef_2].bookmaker:
-                if (1 / current_match[coef_1].coef + 1 / second_bet[coef_2].coef < 1):
-                    fork.append(Fork([current_match[coef_1], second_bet[coef_2]], time.time()))
-        else:
-            for third_bet in matches:
-                if (current_match[coef_1].bookmaker != second_bet[coef_2].bookmaker != third_bet[coef_3].bookmaker):
-                    if (1 / current_match[coef_1].coef + 1 / second_bet[coef_2].coef + 1 / third_bet[coef_3].coef < 1):
-                        fork.append(
-                            Fork([current_match[coef_1], second_bet[coef_2], third_bet[coef_3].coef], time.time()))
-
-    # for match in matches:
-    #
-    #     if 1 / current_match[0].coef + 1 / match[1].coef < 1:
-    #         if current_match[0].coef < match[1].coef:
-    #             fork.append(Fork(current_match[0], time.time()))
-    #             fork.append(Fork(match[1],time.time()))
-    #             print(fork,'a')
-    #         else:
-    #             fork.append(Fork(match[0], time.time()))
-    #             fork.append(Fork(current_match[1], time.time()))
-    #     if 1 / current_match[1].coef + 1 / match[0].coef < 1:
-    #         if current_match[1].coef < match[0].coef:
-    #             fork.append(Fork(current_match[1], time.time()))
-    #             fork.append(Fork(match[0], time.time()))
-    #         else:
-    #             print(match[1])
-    #             fork.append(Fork([match[1], current_match[0]], time.time()))
-    #             fork.append(Fork(current_match[0], time.time()))
-    return fork
+    return res
 
 
 def flat_list(array, new_list=None):
